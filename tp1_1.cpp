a4906605de04c327ad8adcdc231b39b49e97cfa6
josepro752 <joseignacioboggio@gmail.com>
2023-03-20 13:07:55 -0300

    Termine el codigo del punto 3

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main () {
+    int variable = 7;
+    int *punt = &variable;
     printf("Hola Mundo");
+    printf("\nEl contenido del puntero es: %d", *punt);
+    printf("\nLa direccion de memoria almacenada por el puntero es: %d", punt);
+    printf("\nLa direccion de memoria de la variable es: %d", &variable);
+    printf("\nLa direccion de memoria del puntero es: %d", &punt);
+    printf("\nEl tamano de memoria utilizado por la variable es: %d", sizeof(variable));
     return 0;
 }
